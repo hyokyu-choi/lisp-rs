34b40b80870cef43e8823fa530196ae8bc78c1f5
Hyokyu Choi <chlgyrb111@uos.ac.kr>
2025-12-20 17:32:30 +0900

    Refact modules structure

---
+++ src/physics_module/constants.rs
@@ -0,0 +1,2 @@
+pub const PI: f64 = std::f64::consts::PI;
+pub const E: f64 = std::f64::consts::E;
---
+++ src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub fn print_title(title: &str) {
+    println!("=========== {title} ===========")
+}
